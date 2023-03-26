d4860fd7373188138e43ef01b4769031d30981dd
Nikita Titov <titovnik7@yandex.ru>
2023-03-26 23:12:39 +0300

    commit_sprint4

---
+++ src/main/java/model/MainPage.java
@@ -0,0 +1,79 @@
+package model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MainPage {
+    public static final String PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
+    private static final By CREATE_ORDER_BUTTON_TOP = By.xpath(".//button[@class = 'Button_Button__ra12g']");
+    public static final By CREATE_ORDER_BUTTON_BOTTOM = By.xpath(".//button[@class = 'Button_Button__ra12g Button_Middle__1CSJM']");
+    public static final By FIRST_QUESTION = By.id("accordion__heading-0");
+    public static final By SECOND_QUESTION = By.id("accordion__heading-1");
+    public static final By THIRD_QUESTION = By.id("accordion__heading-2");
+    public static final By FOURTH_QUESTION = By.id("accordion__heading-3");
+    public static final By FIFTH_QUESTION = By.id("accordion__heading-4");
+    public static final By SIXTH_QUESTION = By.id("accordion__heading-5");
+    public static final By SEVENTH_QUESTION = By.id("accordion__heading-6");
+    public static final By EIGHTH_QUESTION = By.id("accordion__heading-7");
+    private final WebDriver driver;
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void open() {
+        driver.get(PAGE_URL);
+    }
+
+
+    public void clickCreateOrderButtonTop() {
+
+        driver.findElement(CREATE_ORDER_BUTTON_TOP).click();
+    }
+    public void clickCreateOrderButtonBottom() {
+        WebElement element = driver.findElement(CREATE_ORDER_BUTTON_BOTTOM);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(CREATE_ORDER_BUTTON_BOTTOM).click();
+    }
+    public void clickOnTheFirstQuestion() {
+        WebElement element = driver.findElement(FIRST_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(FIRST_QUESTION).click();
+    }
+    public void clickOnTheSecondQuestion() {
+        WebElement element = driver.findElement(SECOND_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(SECOND_QUESTION).click();
+    }
+    public void clickOnTheThirdQuestion() {
+        WebElement element = driver.findElement(THIRD_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(THIRD_QUESTION).click();
+    }
+    public void clickOnTheFourthQuestion() {
+        WebElement element = driver.findElement(FOURTH_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(FOURTH_QUESTION).click();
+    }
+    public void clickOnTheFifthQuestion() {
+        WebElement element = driver.findElement(FIFTH_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(FIFTH_QUESTION).click();
+    }
+    public void clickOnTheSixthQuestion() {
+        WebElement element = driver.findElement(SIXTH_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(SIXTH_QUESTION).click();
+    }
+    public void clickOnTheSeventhQuestion() {
+        WebElement element = driver.findElement(SEVENTH_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(SEVENTH_QUESTION).click();
+    }
+    public void clickOnTheEighthQuestion() {
+        WebElement element = driver.findElement(EIGHTH_QUESTION);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(EIGHTH_QUESTION).click();
+    }
+
+}
---
+++ src/test/java/TestQuestionsAboutImportant.java
@@ -0,0 +1,42 @@
+import model.MainPage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestQuestionsAboutImportant {
+    private WebDriver driver;
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void checkQuestionsAboutImportant() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+        mainPage.clickOnTheFirstQuestion();
+        Assert.assertEquals("Сутки — 400 рублей. Оплата курьеру — наличными или картой.", driver.findElement(By.id("accordion__panel-0")).getText());
+        mainPage.clickOnTheSecondQuestion();
+        Assert.assertEquals("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", driver.findElement(By.id("accordion__panel-1")).getText());
+        mainPage.clickOnTheThirdQuestion();
+        Assert.assertEquals("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", driver.findElement(By.id("accordion__panel-2")).getText());
+        mainPage.clickOnTheFourthQuestion();
+        Assert.assertEquals("Только начиная с завтрашнего дня. Но скоро станем расторопнее.", driver.findElement(By.id("accordion__panel-3")).getText());
+        mainPage.clickOnTheFifthQuestion();
+        Assert.assertEquals("Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", driver.findElement(By.id("accordion__panel-4")).getText());
+        mainPage.clickOnTheSixthQuestion();
+        Assert.assertEquals("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", driver.findElement(By.id("accordion__panel-5")).getText());
+        mainPage.clickOnTheSeventhQuestion();
+        Assert.assertEquals("Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", driver.findElement(By.id("accordion__panel-6")).getText());
+        mainPage.clickOnTheEighthQuestion();
+        Assert.assertEquals("Да, обязательно. Всем самокатов! И Москве, и Московской области.", driver.findElement(By.id("accordion__panel-7")).getText());
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
